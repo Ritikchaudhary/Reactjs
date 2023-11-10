26ff9c85799881d360b8fd41bf4e82cc88b58ffb
Ritikchaudhary <ritikchaudhary9200@gmail.com>
2023-11-10 15:42:12 +0530

    Inception

--- App.js
+++ App.js
@@ -1,8 +1,6 @@
 const heading = React.createElement("div", {id: "heading"}, React.createElement("div", {id: "child 1"},
 [React.createElement("h1", {},"Heading 1"), React.createElement("h2", {}, "Heading 2")]));
 
-
-
 const root = ReactDOM.createRoot(document.getElementById("head"));
 
 root.render(heading);
