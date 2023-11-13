a2a44939c8a8e553700f0a0ef7fa1e30a12406c6
Ritikchaudhary <ritikchaudhary9200@gmail.com>
2023-11-13 17:55:48 +0530

    Laying foundation

--- App.js
+++ App.js
@@ -1,10 +1,31 @@
 import React from "react"
 import ReactDOM from "react-dom/client"
 
+//React Element
+const heading = <h1>Namastey React using JSX</h1>;
 
-const heading = React.createElement("div", {id: "heading"}, React.createElement("div", {id: "child 1"},
-[React.createElement("h1", {},"Heading 1"), React.createElement("h2", {}, "Heading 2")]));
+//React Components
+const HeadingComponent = ()=>(
+    <h3>Namastey React from Functional component 1</h3>
+);
+
+const HeadingComponent2 = ()=>{
+    return <h2>Namastey React from Functional component 2</h2>;
+};
+
+// using this '{}' brackets we can execute any javascript code inside the React component.
+// diffrent ways to render react components inside the other components or React Element.
+const NestedElements = ()=>(
+    <div id="container">
+        {heading}
+        {HeadingComponent()}
+        <HeadingComponent/>
+        <HeadingComponent2></HeadingComponent2>
+        <h1>Namastey React from container</h1>
+    </div>
+);
 
 const root = ReactDOM.createRoot(document.getElementById("head"));
 
-root.render(heading);
+//Rendering a component
+root.render(<NestedElements/>);
