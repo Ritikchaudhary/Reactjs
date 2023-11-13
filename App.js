e11cffcb8ef46942a7dd754726df49c810fd3202
Ritikchaudhary <ritikchaudhary9200@gmail.com>
2023-11-14 00:04:54 +0530

    Basic Layout React code

--- App.js
+++ App.js
@@ -1,31 +1,77 @@
 import React from "react"
 import ReactDOM from "react-dom/client"
 
-//React Element
-const heading = <h1>Namastey React using JSX</h1>;
 
-//React Components
-const HeadingComponent = ()=>(
-    <h3>Namastey React from Functional component 1</h3>
-);
+const Header = ()=>{
+    return (
+        <div className="header">
+            <div className="logo-container">
+            <img className="logo" src="https://cdn3.vectorstock.com/i/1000x1000/73/07/cooking-and-restaurant-logo-design-vector-29707307.jpg"/>
+            </div>
 
-const HeadingComponent2 = ()=>{
-    return <h2>Namastey React from Functional component 2</h2>;
+            <div className="nav-items">
+                <ul>
+                    <li>Home</li>
+                    <li>About Us</li>
+                    <li>Contact</li>
+                    <li>Cart</li>
+                </ul>
+            </div>
+        </div>
+    );
 };
 
-// using this '{}' brackets we can execute any javascript code inside the React component.
-// diffrent ways to render react components inside the other components or React Element.
-const NestedElements = ()=>(
-    <div id="container">
-        {heading}
-        {HeadingComponent()}
-        <HeadingComponent/>
-        <HeadingComponent2></HeadingComponent2>
-        <h1>Namastey React from container</h1>
-    </div>
-);
+const Card = ()=>{
+    return (
+        <div className="card">
+            <img 
+                className="image"
+                alt = "image"
+                src="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/iivuhjc2mswi9lublktf"
+            />
+            <h2 className="h2tag"> Meghana Foods</h2>
+            <div>
+            <h5 className="h5tag">Biryani, North Indian, Asian</h5>
+            <h5 className="h5tag" >4.4 Stars</h5>
+            <h5 className="h5tag">25 Mins</h5>
+            </div>
+        </div>
+    );
+};
+
+const Body = ()=>{
+    return (
+        <div className="body">
+            <div className="search">Search</div>
+            <div className="card-container">
+                <Card/>
+                <Card/>
+                <Card/>
+                <Card/>
+                <Card/>
+                <Card/>
+                <Card/>
+                <Card/>
+                <Card/>
+                <Card/>
+                <Card/>
+                <Card/>
+                <Card/>
+                <Card/>
+                <Card/>
+            </div>
+        </div>
+    );
+};
+
+const AppContainer = ()=>{
+    return (
+        <div className="App">
+            <Header/>
+            <Body/>
+        </div>
+    );
+};
 
 const root = ReactDOM.createRoot(document.getElementById("head"));
-
-//Rendering a component
-root.render(<NestedElements/>);
+root.render(<AppContainer/>);
