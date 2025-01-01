e9ef4111ea6264a392925609de57c6034773079f
Ritikchaudhary <ritikchaudhary9200@gmail.com>
2025-01-01 16:40:38 +0530

    Updates

---
+++ index.6bd02f5a.js
@@ -0,0 +1,12 @@
+const heading = React.createElement("div", {
+    id: "heading"
+}, React.createElement("div", {
+    id: "child 1"
+}, [
+    React.createElement("h1", {}, "Heading 1"),
+    React.createElement("h2", {}, "Heading 2")
+]));
+const root = ReactDOM.createRoot(document.getElementById("head"));
+root.render(heading);
+
+//# sourceMappingURL=index.6bd02f5a.js.map
